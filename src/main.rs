e8f23dd9c1580db592240a09521a4bbea9984017
Hadiuzzaman <zhhadi50@gmail.com>
2025-05-30 22:05:01 +0600

    learn some string oparation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-
-
 fn main(){
-    let role = "admin";
+    let mut name = String::from("Hadi");
 
-    match role {
-        "admin" => println!("You are wellcome in Admin panel"),
-        "user" => println!("you are wellcome in dashboard"),
-        _ => println!("Wellcome Mr."),
+    // change data in stirng
+    name = "Hadiuzzaman".to_string();
+
+    // apend in string text
+    name.push_str(" Hadi");
+
+    // apand word
+    name.push('!');
+
+    // replace word
+    let new_name = name.replace("Hadi!", "Hadi");
+
+    println!("{}", name);
+    println!("{}", new_name);
+  
+
+    // print every word
+    let word = name.split_whitespace();
+    println!("{:?}", word);
+
+    for ch in name.split_whitespace(){
+        println!("{}", ch);
     }
+
+    // count the sentence length
+    println!("Length in bytes: {}", name.len());
+    println!("Number of characters : {}", name.chars().count());
 }
