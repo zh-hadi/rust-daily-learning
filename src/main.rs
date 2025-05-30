886943d5e7a5c4ad317c83724a5240fbe5bb974f
Hadiuzzaman <zhhadi50@gmail.com>
2025-05-30 21:42:49 +0600

    match feature example in rust

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
-#[derive(Debug)]
-struct Product {
-    name: String,
-    price: i32,
-}
 
-impl Product{
-    fn new(name: String, price: i32)->Self{
-        Self{name, price}
-    }
-
-    fn info(&self){
-        println!("product name: {}\nPrice: {}", self.name, self.price)
-    }
-
-    fn s_print(&self){
-        println!("{:?}", self);
-    }
-
-    fn change_name(&mut self, name: String){
-        self.name = name;
-    }
-}
 
 fn main(){
-    let mut product1 = Product::new("pant".to_string(), 20);
-    product1.s_print();
-    product1.change_name("T-Shirt".to_string());
-    product1.info();
-    product1.s_print();
+    let role = "admin";
+
+    match role {
+        "admin" => println!("You are wellcome in Admin panel"),
+        "user" => println!("you are wellcome in dashboard"),
+        _ => println!("Wellcome Mr."),
+    }
 }
